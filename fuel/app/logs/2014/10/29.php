4db07b02ab8fbe61bcf3a721e61b331493935ce7
pham-thuan-mulodo <pham.thuan@mulodo.com>
2014-10-29 17:46:31 +0700

    design init test case

--- fuel/app/logs/2014/10/29.php
+++ fuel/app/logs/2014/10/29.php
@@ -675,3 +675,4 @@ INFO - 2014-10-29 10:49:54 --> Fuel\Core\Request::__construct - Creating a new m
 INFO - 2014-10-29 10:49:54 --> Fuel\Core\Request::execute - Called
 INFO - 2014-10-29 10:49:54 --> Fuel\Core\Request::execute - Setting main Request
 DEBUG - 2014-10-29 10:49:54 --> ID of comment to edit now = 4
+WARNING - 2014-10-29 11:14:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
---
+++ fuel/app/tests/controller/user.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @group UserAPI
+ */
+
+class Test_Controller_User extends TestCase {
+    public function setUp() {
+    }
+    
+    public function tearDown() {
+    }
+    
+    public function test_post_login() {
+        
+    }
+}
+
