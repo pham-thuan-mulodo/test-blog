2d18525a010c444ace3a37961dfa736c0a5794fa
pham-thuan-mulodo <pham.thuan@mulodo.com>
2014-10-29 09:04:57 +0700

    add log file

---
+++ fuel/app/logs/2014/10/29.php
@@ -0,0 +1,11 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-10-29 03:01:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:02:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:02:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:02 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-29 03:03:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
