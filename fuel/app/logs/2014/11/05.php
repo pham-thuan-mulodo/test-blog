c5730b3e21b2c282f6a80cfd4cefa50b22f18ed8
pham-thuan-mulodo <pham.thuan@mulodo.com>
2014-11-05 08:57:31 +0700

    add log file

---
+++ fuel/app/logs/2014/11/05.php
@@ -0,0 +1,26 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-11-05 02:56:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+INFO - 2014-11-05 02:56:35 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "user_logout"
+INFO - 2014-11-05 02:56:35 --> Fuel\Core\Request::execute - Called
+INFO - 2014-11-05 02:56:35 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2014-11-05 02:56:35 --> You are not login yet.
+DEBUG - 2014-11-05 02:56:35 --> You cannot logout because invalid token
+WARNING - 2014-11-05 02:56:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+INFO - 2014-11-05 02:56:36 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "user_logout"
+INFO - 2014-11-05 02:56:36 --> Fuel\Core\Request::execute - Called
+INFO - 2014-11-05 02:56:36 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2014-11-05 02:56:36 --> You are not login yet.
+DEBUG - 2014-11-05 02:56:36 --> You cannot logout because invalid token
+WARNING - 2014-11-05 02:56:41 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+INFO - 2014-11-05 02:56:41 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "user_edit"
+INFO - 2014-11-05 02:56:41 --> Fuel\Core\Request::execute - Called
+INFO - 2014-11-05 02:56:41 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2014-11-05 02:56:41 --> You are not login yet.
+DEBUG - 2014-11-05 02:56:41 --> You cannot edit user because of invalid token
+WARNING - 2014-11-05 02:56:42 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+INFO - 2014-11-05 02:56:42 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "user_edit"
+INFO - 2014-11-05 02:56:42 --> Fuel\Core\Request::execute - Called
+INFO - 2014-11-05 02:56:42 --> Fuel\Core\Request::execute - Setting main Request
+ERROR - 2014-11-05 02:56:42 --> You are not login yet.
+DEBUG - 2014-11-05 02:56:42 --> You cannot edit user because of invalid token
